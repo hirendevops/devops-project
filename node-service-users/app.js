5c944fecbb81c94b3e60f15c6b8009fa446ba339
hiren <hirendevops1991@gmail.com>
2025-10-06 18:18:40 +0530

    Remove node_modules and add .gitignore

---
+++ node-service-users/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
+
+module.exports = app;
